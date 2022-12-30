b6065bc9a2f776c3fc1c68a27532617292ece809
KingPra <kingi.pra@gmail.com>
2022-12-30 18:30:49 -0500

    added mergeSort

--- algos/selectionSort.js
+++ algos/selectionSort.js
@@ -1,4 +1,14 @@
 
+//Space Time complexity = O(n^2)T, O(1)S
+/**
+ * 
+ * @param {The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from the unsorted part and putting it at the beginning. 
+The algorithm maintains two subarrays in a given array.
+The subarray which already sorted. 
+The remaining subarray was unsorted.
+In every iteration of the selection sort, the minimum element (considering ascending order) from the unsorted subarray is picked and moved to the sorted subarray. } arr 
+ * 
+ */
 function selectionSort(arr){
 
     for(let i = 0; i < arr.length; i++) {
