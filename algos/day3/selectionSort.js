dd7cb628c827811b5260a9c5ed83f05bf404ec07
KingPra <kingi.pra@gmail.com>
2023-01-05 19:21:06 -0500

    day3 updates

--- algos/day3/selectionSort.js
+++ algos/day3/selectionSort.js
@@ -1,23 +1,22 @@
 function selectionSort(arr) {
-    let length = arr.length;
-    for(let i = 1, j = 0; i < length; i++) {
+  let length = arr.length;
 
-        for(let i = 0; i < length; i++) {
-            let minIdx = i;
-            for(let j = i + 1; j < length; j++) {
-                if(arr[minIdx] > arr[j]) {
-                    minIdx = j;
-                }
-            }
-
-            if(minIdx !== i) {
-                const temp = arr[i];
-                arr[i] = arr[minIdx];
-                arr[minIdx] = temp;
-            }
-        }
+  for (let i = 0; i < length; i++) {
+    let minIdx = i;
+    for (let j = i + 1; j < length; j++) {
+      if (arr[minIdx] > arr[j]) {
+        minIdx = j;
+      }
     }
-    return arr;
+
+    if (minIdx !== i) {
+      const temp = arr[i];
+      arr[i] = arr[minIdx];
+      arr[minIdx] = temp;
+    }
+  }
+
+  return arr;
 }
 
-console.log(selectionSort([2,1,3,5,4,8,7,6]));
+console.log(selectionSort([2, 1, 3, 5, 4, 8, 7, 6]));
